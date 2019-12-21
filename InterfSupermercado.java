30faf3190a80d3f320099f9b4f458ea612942a00
rafigueredo <51867659+rafigdcomp@users.noreply.github.com>
2019-12-21 15:03:46 -0300

    Add files via upload
    Interface do Projeto Supermercado

---
+++ InterfSupermercado.java
@@ -0,0 +1,15 @@
+import java.util.List;
+
+public interface InterfSupermercado{
+
+    public void insereCliente();
+    public void insereVendedor();
+    public void insereGerente();
+    public void insereProdutos();
+    public void controleDeEstoques();
+    public void vendaDeProdutos();
+    public void pesquisaDeProdutos();
+    public double pagamentoDasVendas();
+    public void relatoioDeVendas();
+    public void relatorioDeEstoques();
+}
